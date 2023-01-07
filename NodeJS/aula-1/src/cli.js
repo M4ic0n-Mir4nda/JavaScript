f3e1826bf56e0571404d187d73e98bcefa953c65
M4ic0n-Mir4nda <107010440+M4ic0n-Mir4nda@users.noreply.github.com>
2023-01-06 22:55:47 -0300

    Update

--- NodeJS/aula-1/src/cli.js
+++ NodeJS/aula-1/src/cli.js
@@ -1,11 +1,41 @@
-import chalk from 'chalk'
+import chalk from 'chalk';
+import fs from 'fs';
 import pegaArquivo from "./index.js";
 
 const caminho = process.argv; // Valores de argumento - O objeto process captura o que foi passado para a linha de comando para ser usado internamente pelo programa
 
-async function processaTexto(caminho) {
-    const resultado = await pegaArquivo(caminho[2]);
-    console.log(chalk.yellow('Lista de links'), resultado);
+function imprimeLista(resultado, arquivo = '') {
+    console.log(
+        chalk.yellow('lista de links'), 
+        chalk.blueBright(arquivo),
+        resultado);
 }
 
-processaTexto(caminho)
+async function processaTexto(argumentos) {
+    const caminho = argumentos[2];
+
+    try {
+        fs.lstatSync(caminho);
+    } catch (erro) {
+       if (erro.code === 'ENOENT') {
+        console.log('arquivo ou diretório não existe');
+        return;
+       }
+    }
+
+        // essa função da lib fs serve para verificar se o argumento passado é um arquivo e retorna true ou false
+    if (fs.lstatSync(caminho).isFile()) {
+        const resultado = await pegaArquivo(argumentos[2]);
+        imprimeLista(resultado)
+        // essa função da lib fs serve para verificar se o argumento passado é um diretorio e retorna true ou false
+    } else if (fs.lstatSync(caminho).isDirectory()) {
+        // readdir === le o diretorio e é passado como parametro o que está chegando nele
+        const arquivos = await fs.promises.readdir(caminho)
+        arquivos.forEach(async (nomeDeArquivo) => {
+            const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
+            imprimeLista(lista, nomeDeArquivo);
+        })
+    }
+}
+
+processaTexto(caminho);
