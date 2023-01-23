7df9d5499300a213f4b1326c0c8fdec1ef382f98
M4ic0n-Mir4nda <maiconmirandasantana@gmail.com>
2023-01-22 21:56:08 -0300

    Update

--- JavaScript-Front-end/Alura/Mochila de Viagem/js/main.js
+++ JavaScript-Front-end/Alura/Mochila de Viagem/js/main.js
@@ -1,23 +1,58 @@
 const form = document.querySelector('#novoItem');
 const lista = document.querySelector('#lista')
+const itens = JSON.parse(localStorage.getItem('itens')) || []
+            // Transforma a string convertida na função em um objeto para fazer a consulta no array
+
+itens.forEach((elemento) => {
+    criaElemento(elemento)
+})
 
 form.addEventListener('submit', (event) => {
-    criaElemento(event.target.elements['nome'].value, event.target.elements['quantidade'].value)
+    const nomeItem = event.target.elements['nome']
+    const quantidadeItem = event.target.elements['quantidade']
+
+    const existe = itens.find( elemento => elemento.nome === nomeItem.value )
+    
+    const itemAtual = {
+        "nome": nomeItem.value,
+        "quantidade": quantidadeItem.value
+    }
+
+    if (existe) {
+        itemAtual.id = existe.id
+        atualizaElemento(itemAtual)
+    } else {
+        itemAtual.id = itens.length
+
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
+    }
+    
+                                // Transforma o objeto em uma string pois o localStorage só salva textos
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+    nomeItem.value = ""
+    quantidadeItem.value = ""
 
     event.preventDefault()
 })
 
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {
     const novoItem = document.createElement('li');
     novoItem.classList.add('item')
 
     const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
-
+    numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id 
     novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
+    novoItem.innerHTML += item.nome
 
     lista.appendChild(novoItem)
 
     // Forma simplificada > lista.innerHTML += `<li class="item"><strong>${quantidade}</strong>${nome}</li>`
+}
+
+function atualizaElemento(item) {
+    document.querySelector(`[data-id="${item.id}"]`).innerHTML = item.quantidade
 }
---
+++ NodeJS/POO/OO com JavaScript/objetoLiteral.js
@@ -0,0 +1,33 @@
+const user = {
+    nome: 'Maicon',
+    email: 'm@m.com',
+    nascimento: '2001/05/31',
+    role: 'estudante',
+    ativo: true,
+    exibirInfos: function() {
+        console.log(this.nome, this.email)
+    }
+}
+/*
+const exibir = function() {
+    console.log(this.nome)
+}
+
+                        // O bind prende ou segura a execução de uma função ou contexto especifico
+const exibirNome = exibir.bind(user)
+exibirNome()
+exibir()
+*/
+
+const admin = {
+    nome: 'Maria',
+    email: 'm@m.com',
+    role: 'admin',
+    criaCurso() {
+        console.log('curso criado!')
+    }
+}
+
+Object.setPrototypeOf(admin, user)
+admin.criaCurso()
+admin.exibirInfos()
