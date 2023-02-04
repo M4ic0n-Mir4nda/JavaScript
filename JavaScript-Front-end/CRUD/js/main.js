5dd002aa57adf2dc071085f025e041b2786e3f63
M4ic0n-Mir4nda <maiconmirandasantana@gmail.com>
2023-02-04 00:42:06 -0300

    Update

--- JavaScript-Front-end/CRUD/js/main.js
+++ JavaScript-Front-end/CRUD/js/main.js
@@ -1,36 +1,39 @@
 const form = document.querySelector('#form');
+const buttonCadastro = document.querySelector('#button-cadastrar');
+const divTabela = document.querySelector('#cadastrados');
 const tabela = document.querySelector('#tabela');
-const divTabela = document.querySelector('#cadastrados')
-const cadastrados = JSON.parse(localStorage.getItem('cadastrados')) || []
+const cadastrados = JSON.parse(localStorage.getItem('cadastrados')) || [];
 
 cadastrados.forEach( (elemento) => {
     criaElemento(elemento)
 })
 
-// let tr = tabela.getElementsByTagName('tr');
+buttonCadastro.addEventListener('click', (e) => {
 
-// if (tr.length > 2) {
-//     divTabela.style.display = 'block'
-// } 
+    form.style.display = 'block'
+    divTabela.style.display = 'none'
+
+    e.preventDefault()
+})
 
 form.addEventListener('submit', (e) => {
 
     const nome = e.target.elements['nome'];
-    const contato = e.target.elements['contato'];
     const cep = e.target.elements['cep'];
+    const cidade = e.target.elements['cidade'];
+    const bairro = e.target.elements['bairro'];
     const rua = e.target.elements['rua'];
-    const complemento = e.target.elements['complemento'];
     const numero = e.target.elements['numero'];
-    const cidade = e.target.elements['cidade']
+    const complemento = e.target.elements['complemento'];
 
     const pessoa = {
         "nome": nome.value,
-        "contato": contato.value,
         "cep": cep.value,
+        "cidade": cidade.value,
+        "bairro": bairro.value,
         "rua": rua.value,
-        "complemento": complemento.value,
         "numero": numero.value,
-        "cidade": cidade.value
+        "complemento": complemento.value,
     }
 
     const existe = cadastrados.find( elemento => elemento.nome === pessoa.nome)
@@ -38,54 +41,58 @@ form.addEventListener('submit', (e) => {
     if (existe) {
         alert('Usúario ja cadastrado')
     } else {
-        pessoa.id = cadastrados.length
+        pessoa.id = cadastrados[cadastrados.length - 1] ? cadastrados[cadastrados.length - 1].id + 1 : 0
         criaElemento(pessoa)
 
         cadastrados.push(pessoa)
 
         nome.value = ''
-        contato.value = ''
         cep.value = ''
-        rua.value = ''
-        complemento.value = ''
-        numero.value = ''
         cidade.value = ''
-        
+        bairro.value = ''
+        rua.value = ''
+        numero.value = ''
+        complemento.value = ''
+
+        form.style.display = 'none'
+
+        divTabela.style.display = 'block'    
     }
 
     localStorage.setItem("cadastrados", JSON.stringify(cadastrados))
 
     e.preventDefault()
+
 })
 
 function criaElemento(item) {
     const novaColuna = document.createElement('tr')
     
     const elementoNome = document.createElement('td');
-    const elementoContato = document.createElement('td');
-    const elementoIdade = document.createElement('td');
     const elementoCep = document.createElement('td');
+    const elementoCidade = document.createElement('td');
+    const elementoBairro = document.createElement('td');
     const elementoRua = document.createElement('td');
-    const elementoComplemento = document.createElement('td');
     const elementoNumero = document.createElement('td');
+    const elementoComplemento = document.createElement('td');
 
     novaColuna.dataset.id = item.id
 
-    elementoNome.innerHTML = item.nome
-    elementoContato.innerHTML = item.contato
-    elementoIdade.innerHTML = item.cidade
-    elementoCep.innerHTML = item.cep
-    elementoRua.innerHTML = item.rua
-    elementoComplemento.innerHTML = item.complemento
-    elementoNumero.innerHTML = item.numero
+    elementoNome.innerHTML = item.nome;
+    elementoCep.innerHTML = item.cep;
+    elementoCidade.innerHTML = item.cidade;
+    elementoBairro.innerHTML = item.bairro;
+    elementoRua.innerHTML = item.rua;
+    elementoNumero.innerHTML = item.numero;
+    elementoComplemento.innerHTML = item.complemento;
 
     novaColuna.appendChild(elementoNome);
-    novaColuna.appendChild(elementoContato);
-    novaColuna.appendChild(elementoIdade);
     novaColuna.appendChild(elementoCep);
+    novaColuna.appendChild(elementoCidade);
+    novaColuna.appendChild(elementoBairro);
     novaColuna.appendChild(elementoRua);
+    novaColuna.appendChild(elementoNumero);
     novaColuna.appendChild(elementoComplemento);
-    novaColuna.appendChild(elementoNumero)
    
     novaColuna.appendChild(criaBotaoDelete(item.id))
     novaColuna.appendChild(criaBotaoEdit(item.id))
@@ -95,11 +102,11 @@ function criaElemento(item) {
 
 function criaBotaoDelete(id) {
     const buttonDelete = document.createElement('button');
-    buttonDelete.innerText = 'Excluir'
-    buttonDelete.classList.add('buttonExcluir')
+    buttonDelete.innerText = 'Excluir';
+    buttonDelete.classList.add('buttonExcluir');
 
     buttonDelete.addEventListener('click', function() {
-        deletaElemento(this.parentNode, id)
+        deletaElemento(this.parentNode, id);
     })
 
     return buttonDelete;
@@ -107,16 +114,43 @@ function criaBotaoDelete(id) {
 
 function criaBotaoEdit(id) {
     const buttonEdit = document.createElement('button');
-    buttonEdit.innerText = 'Editar'
-    buttonEdit.classList.add('buttonEditar')
+    buttonEdit.innerText = 'Editar';
+    buttonEdit.classList.add('buttonEditar');
 
     return buttonEdit;
 }
 
 function deletaElemento(tag, id) {
-    tag.remove()
+    tag.remove();
 
-    cadastrados.splice(cadastrados.findIndex( elemento => elemento.id === id), 1)
+    cadastrados.splice(cadastrados.findIndex( elemento => elemento.id === id), 1);
 
-    localStorage.setItem('cadastrados', JSON.stringify(cadastrados))
+    localStorage.setItem('cadastrados', JSON.stringify(cadastrados));
 }
+
+async function validaCep(cep) {
+    try {
+        const request = await fetch(`https://viacep.com.br/ws/${cep.value}/json/`);
+        const response = await request.json();
+        if (response.erro) {
+            alert('CEP não existente!')
+            throw Error('CEP não existente!');
+        }
+        document.querySelector('#rua').value = response.logradouro
+        document.querySelector('#bairro').value = response.bairro;
+        document.querySelector('#cidade').value = response.localidade;
+        return response;
+    } catch (erro) {
+        console.log(erro);
+    }
+}
+
+function mascaraCep(cep) {
+    if (cep.value.length == 5) {
+        cep.value = cep.value + '-'
+    }
+}
+
+
+
+
