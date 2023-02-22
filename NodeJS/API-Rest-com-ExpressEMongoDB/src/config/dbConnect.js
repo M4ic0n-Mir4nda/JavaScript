e6240f0dee64ec15a1bd187f755c74ec9f27cbf7
M4ic0n-Mir4nda <107010440+M4ic0n-Mir4nda@users.noreply.github.com>
2023-02-22 07:32:09 -0300

    Update dbConnect.js
    Update

--- NodeJS/API-Rest-com-ExpressEMongoDB/src/config/dbConnect.js
+++ NodeJS/API-Rest-com-ExpressEMongoDB/src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://maicon:mitaki2912@crud-node.hkf0h4t.mongodb.net/alura-node?"); // Configuração de conexao com o banco
+mongoose.connect("mongodb+srv://username:password@crud-node.hkf0h4t.mongodb.net/alura-node?"); // Configuração de conexao com o banco
 
 let db = mongoose.connection; // variavel para fazer a comunicação da aplicação com o banco de dados
 
