51b5b490d8cf7b8f30f2e687344b42a08630eadb
M4ic0n-Mir4nda <107010440+M4ic0n-Mir4nda@users.noreply.github.com>
2023-03-14 19:53:22 -0300

    Update dbConnect.js

--- NodeJS/Express/src/config/dbConnect.js
+++ NodeJS/Express/src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://maicon:mitaki2912@crud-node.hkf0h4t.mongodb.net/alura-node?");
+mongoose.connect("mongodb+srv://user:password@crud-node.hkf0h4t.mongodb.net/alura-node?");
 
 const db = mongoose.connection;
 
