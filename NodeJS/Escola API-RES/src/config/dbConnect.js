eb5c7bc709f25a184bab60adc2998d78498a0a4e
M4ic0n-Mir4nda <maiconmirandasantana@gmail.com>
2023-03-24 23:11:49 -0300

    Update

--- NodeJS/Escola API-RES/src/config/dbConnect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect("mongodb+srv://maicon:mitaki2912@escola.q5ibel6.mongodb.net/cadastro-escolar?");
-
-let db = mongoose.connection;
-
-export default db;
