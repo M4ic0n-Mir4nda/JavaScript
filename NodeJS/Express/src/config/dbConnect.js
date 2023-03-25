457e185f7ba819fc8ba74395a4e60b8b163383f1
M4ic0n-Mir4nda <maiconmirandasantana@gmail.com>
2023-03-24 22:34:16 -0300

    Update

--- NodeJS/Express/src/config/dbConnect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect("mongodb+srv://maicon:mitaki2912@crud-node.hkf0h4t.mongodb.net/alura-node?");
-
-const db = mongoose.connection;
-
-export default db;
