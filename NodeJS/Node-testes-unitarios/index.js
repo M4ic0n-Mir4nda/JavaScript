600604feda6a22c29da41a4849cef9109ac796f2
M4ic0n-Mir4nda <maiconmirandasantana@gmail.com>
2024-05-28 17:41:47 -0300

    Update files

--- NodeJS/Node-testes-unitarios/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-const somasHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
-
-const calculaDescontos = (salario, descontos) => salario - descontos;
-
-export {
-  somasHorasExtras,
-  calculaDescontos,
-};
